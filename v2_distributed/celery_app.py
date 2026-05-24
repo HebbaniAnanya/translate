14145e3eb61014ef0635bde5715941d2c8817a89
HebbaniAnanya <ananya.is21@bmsce.ac.in>
2026-05-24 22:51:44 +0530

    second commit

--- v2_distributed/celery_app.py
+++ v2_distributed/celery_app.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 from playhouse.pwasyncio import AsyncSqliteDatabase
 
-# Define the message broker location (Redis)
+# message broker location (Redis)
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
 # Initialize Celery
@@ -12,7 +12,7 @@ celery_app = Celery(
     backend=REDIS_URL
 )
 
-# Optional: Celery configuration tweaks
+# Celery configuration tweaks
 celery_app.conf.update(
     task_track_started=True,
     timezone='UTC'
